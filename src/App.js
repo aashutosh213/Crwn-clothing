f3d482e219380ef89cde63e89c0433c16615812a
aashutosh213 <aashutosh213@gmail.com>
2021-06-15 18:53:32 +0530

    Update the project with Shop page and add ColletionItem and collctionPreview component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage';
+import ShopPage from './Pages/ShopPage/ShopPage';
 
 
-
-const HatsPage = () => (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
